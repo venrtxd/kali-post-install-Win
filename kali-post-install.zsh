ebee0e9c97a9048c93ea79a76d0210ee7d35625f
CBandal <bambi6650@gmail.com>
2022-06-14 14:13:32 +1000

    Added VSCode

--- kali-post-install.zsh
+++ kali-post-install.zsh
@@ -181,4 +181,11 @@ echo "#### PDFcrack Installed ####"
 echo 'export PATH=/home/user2/.local/bin:$PATH' >> ~/.zshrc
 . ~/.zshrc
 
+## Visual Studio Code
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+echo "#### Visual Studio Code Installed ###"
+
 echo "####  Finished  ####"
